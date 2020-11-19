c9eeab93c2981a19160082890e15fa705fa07ee7
sunjxan <sunjxan@gmail.com>
2020-11-20 03:39:28 +0800

    修改检测颜色

--- 5.物体检测/ball_tracking.py
+++ 5.物体检测/ball_tracking.py
@@ -10,17 +10,18 @@ from imutils.video_capture import playVideo
 from imutils.video_capture import captureCamera
 
 def captureFunc(frame, frameIndex):
-    greenLower = (10, 120, 30)
-    greenUpper = (30, 255, 150)
+    greenLower = (10, 80, 40)
+    greenUpper = (40, 180, 140)
 
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (5, 5), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HLS)
 
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None, iterations=2)
 
-    cv2.imshow('', mask)
+    output = cv2.bitwise_and(frame, frame, mask=mask)
+    cv2.imshow('', output)
 
 def endFunc():
     cv2.destroyAllWindows()
