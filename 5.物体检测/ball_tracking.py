7b34d0090bf14f15deb79cc414a1955fbd5d878c
sunjxan <sunjxan@gmail.com>
2020-11-20 11:01:57 +0800

    物体检测调试

--- 5.物体检测/ball_tracking.py
+++ 5.物体检测/ball_tracking.py
@@ -4,14 +4,18 @@ sys.path.append("..")
 
 import numpy as np
 import cv2
+import math
+from collections import deque
 
 import imutils
 from imutils.video_capture import playVideo
 from imutils.video_capture import captureCamera
 
 def captureFunc(frame, frameIndex):
-    greenLower = (10, 80, 40)
-    greenUpper = (40, 180, 140)
+    greenLower = (15, 100, 60)
+    greenUpper = (35, 160, 120)
+    ptSize = 128
+    pts = deque(maxlen=ptSize)
 
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (5, 5), 0)
@@ -21,17 +25,28 @@ def captureFunc(frame, frameIndex):
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None, iterations=2)
 
     cnts, hier = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    center = None
     if len(cnts) > 0:
         maxContour = max(cnts, key=cv2.contourArea)
         M = cv2.moments(maxContour)
         cX = round(M["m10"] / M["m00"])
         cY = round(M["m01"] / M["m00"])
-        x, y, r = cv2.minEnclosingCircle(maxContour)
-        print(type(x), type(y), type(r))
+        center = (cX, cY)
+        (x, y), r = cv2.minEnclosingCircle(maxContour)
+        x, y, r = round(x), round(y), round(r)
 
         if r > 10:
             cv2.circle(frame, (x, y), r, (0, 255, 255), 2)
-            cv2.circel(frame, (cX, cY), 5, (0, 0, 255), -1)
+            cv2.circle(frame, (cX, cY), 5, (0, 0, 255), -1)
+    pts.appendleft(center)
+
+    count = len(pts)
+    for i in range(1, count):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+
+        thickness = round(math.sqrt(ptSize / (i + 1)) * 2.5)
+        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
     cv2.imshow('', frame)
 
